4a92793eb8c34a8a50e991c147693b02b4457059
Gagan <business@devgagan.in>
2025-07-27 15:19:04 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,7 @@
+# basic bro
+import os
+
+API_ID = int(os.getenv("API_ID", ""))
+API_HASH = os.getenv("API_HASH", "")
+SESSION_STRING = os.getenv("SESSION_STRING", "")
+UNPLASH_API = os.getenv("UNPLASH_API", "")
